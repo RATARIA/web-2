5375eff73a47ec2b0350b9d7508541c02d67791d
RATARIA <arrthurlima1@Outlook.com>
2021-10-27 02:01:09 -0300

    adding kit breeze

--- firstapp/routes/web.php
+++ firstapp/routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/bob', function () {
     return view('bob');
-});
+})->middleware(['guest'])->name('bob');
+
+require __DIR__.'/auth.php';
